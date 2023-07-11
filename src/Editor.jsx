1e9c23b7f969eeda263dd645a5febaed4e09845f
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2023-07-11 11:12:36 +0530

    update changes

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -3,6 +3,8 @@ import JoditEditor from "jodit-react";
 import { Base64 } from "js-base64";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
+import insert_page_break from "./split-pages.png";
+import "./App.css";
 
 function Editor() {
   const { id: documentId } = useParams();
@@ -68,29 +70,116 @@ function Editor() {
     };
   }, [socket, content]);
 
-  const config = useMemo(
+  // const config = useMemo(
+  //   () => ({
+  //     readonly: false,
+  //     // readonly: true,
+  //     // toolbar: false,
+  //     pageBreak: {
+  //       separator: "<!-- pagebreak -->",
+  //     },
+  //     allowResizeX: false,
+  //     allowResizeY: false,
+  //     placeholder: "Start typings...",
+  //     uploader: {
+  //       insertImageAsBase64URI: true,
+  //     },
+  //     removeButtons: ["eraser", "about"],
+  //     controls: {
+  //       font: {
+  //         list: {
+  //           "": "Default",
+  //           "Roboto Medium,Arial,sans-serif": "Roboto",
+  //         },
+  //       },
+  //     },
+  //   }),
+  //   []
+  // );
+  const insertPageBreak = (editor) => {
+    const pageBreakElement = editor.ownerDocument.createElement("div");
+    pageBreakElement.className = "page-break html2pdf__page-break";
+    pageBreakElement.innerHTML = "Page Brake";
+    pageBreakElement.setAttribute("contenteditable", "false");
+    editor.selection.insertNode(pageBreakElement);
+  };
+
+  const pageBreak = {
+    name: "page Break",
+    iconURL: insert_page_break,
+    tooltip: "Insert Page Break",
+    exec: insertPageBreak,
+  };
+  const buttons = [
+    "undo",
+    "redo",
+    "|",
+    "bold",
+    "strikethrough",
+    "underline",
+    "italic",
+    "|",
+    "superscript",
+    "subscript",
+    "|",
+    "align",
+    "|",
+    "ul",
+    "ol",
+    "outdent",
+    "indent",
+    "|",
+    "font",
+    "fontsize",
+    "brush",
+    "paragraph",
+    "|",
+    "image",
+    "link",
+    "table",
+    "|",
+    "hr",
+    "eraser",
+    "copyformat",
+    "|",
+    "fullsize",
+    "selectall",
+    "print",
+    "|",
+    "source",
+    "|",
+    pageBreak,
+    // {
+    //   name: "exportToPdf",
+    //   icon: "",
+    //   tooltip: "Export to Pdf",
+    //   exec: exportToPdf,
+    // },
+  ];
+  const editorConfig = useMemo(
     () => ({
       readonly: false,
-      // readonly: true,
-      // toolbar: false,
-      pageBreak: {
-        separator: "<!-- pagebreak -->",
-      },
-      allowResizeX: false,
-      allowResizeY: false,
-      placeholder: "Start typings...",
+      toolbar: true,
+      spellcheck: true,
+      language: "en",
+      toolbarButtonSize: "medium",
+      toolbarAdaptive: false,
+      showCharsCounter: true,
+      showWordsCounter: true,
+      showXPathInStatusbar: false,
+      askBeforePasteHTML: true,
+      askBeforePasteFromWord: true,
+      //defaultActionOnPaste: "insert_clear_html",
+      buttons: buttons,
       uploader: {
         insertImageAsBase64URI: true,
       },
-      removeButtons: ["eraser", "about"],
-      controls: {
-        font: {
-          list: {
-            "": "Default",
-            "Roboto Medium,Arial,sans-serif": "Roboto",
-          },
-        },
+      font: {
+        selected: "Arial",
       },
+      overflow: false,
+      width: 816,
+      height: 1056,
     }),
     []
   );
@@ -100,7 +189,7 @@ function Editor() {
       <JoditEditor
         ref={editor}
         value={content}
-        config={config}
+        config={editorConfig}
         tabIndex={1}
         onBlur={(newContent) => setContent(newContent)}
         onChange={(newContent) => setContent(newContent)}
